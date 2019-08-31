63d7f26ff10c356727fa831b6ba3d9e2348e04a0
aditya <adityajn105@gmail.com>
2019-08-31 14:53:44 +0530

    download code modified

--- download_data.sh
+++ download_data.sh
@@ -1,22 +1,23 @@
+
 # create a directory
 mkdir brain_tumor_dataset
 cd brain_tumor_dataset
 
 # download the dataset
-wget -O temp.zip https://ndownloader.figshare.com/articles/1512427/versions/5
+wget -O temp.zip -q https://ndownloader.figshare.com/articles/1512427/versions/5
 
 # unzip the dataset and delete the zip
 unzip -q temp.zip && rm temp.zip
 
 # concatenate the multiple zipped data in a single zip
 cat brainTumorDataPublic_* > brainTumorDataPublic_temp.zip
-zip -FF brainTumorDataPublic_temp.zip --out data.zip
+zip -FF brainTumorDataPublic_temp.zip --out data.zip > progress.txt
 
 # remove the temporary files
 rm brainTumorDataPublic_*
 
 # unzip the full archive and delete it 
-unzip data.zip -d data && rm data.zip
+unzip -q data.zip -d data && rm data.zip
 
 # check that "data" contains 3064 files
 ls data | wc -l
